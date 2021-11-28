1375d042cffdd4836c39097017ba05adf38bd7bf
Dude <michael_runschke@gmx.de>
2021-11-28 14:21:43 +0100

    Kapselung von Codeblöcken

--- script_01.js
+++ script_01.js
@@ -3,94 +3,10 @@
 // 1. Kapselung von Codeblöcken
 
 // Funktionsaufruf (call)
-// test();
+test();
 
 // Funktionsrumpf (body) | callee
-function test()
+function test() 
 {
-    console.log("Hallo Anton!");
-}
-
-
-/***** Funktionen 02a *****/
-// 2a. Parametrisierung + Datenübergabe von INNEN
-
-// Aufruf (call)
-// ausgabeNamen(); 
-
-// Funktion
-function ausgabeNamen() 
-{
-    let firstName = "Patrick"; // what happens in Vegas, ...
-    console.log("Hallo " + firstName + "!");
-    console.log(firstName);
-}
-
-//console.log(firstName);  // .. stays in Vegas!
-
-/***** Funktionen 02b *****/
-// 2b. Parametrisierung + Datenübergabe von AUSSEN
-
-function ausgabeNamenParam(firstName) {
-   
-    const cond1 = (firstName == "");
-    const cond2 = (firstName == undefined);
-   
-    if(cond1 || cond2)
-    {
-        firstName = "Nobody";
-    }
-  
-    //let firstName = "Patrick"; Variablen --> Parameter
-    console.log("Hallo " + firstName + "!");
-}
-
-// ausgabeNamenParam("Anton"); // Argument(e) / args
-// ausgabeNamenParam("Milda");
-// ausgabeNamenParam();
-// ausgabeNamenParam(prompt("Vorname?:"));
-
-/***** Funktionen 02c *****/
-// 2c. Mehrere Parameter
-
-function ausgabeNamenParams(firstName,familyName) {
-    console.log("Hallo " + firstName + " " + familyName +"!"); 
-}
-
-// ausgabeNamenParams("Max","Mütze");
-// ausgabeNamenParams(prompt("Vorname?"),prompt("Nachname?"));
-
-/***** Funktionen 03a *****/
-// 03a. Vorbereitung
-// Postulat: one function = one job (uncle Bob)
-// SRP single responsibility principle
-
-function ausgabeNamenParams2(firstName,familyName) {
-
-    // 1. Funktionalität: string composing
-    const gap = " ";
-    const outputStr = "Hallo" + gap + firstName + gap + familyName +"!";
-
-    // 2. Funktionalität: data output
-    console.log(outputStr);
-
-}
-
-// ausgabeNamenParams2("Max","Mütze");
-
-/***** Funktionen 03b *****/
-
-// 1. Funktionalität: string composing
-ausgabe(getString("Max","Mütze"));
-function getString(firstName,familyName) {
-    const gap = " ";
-    const outputStr = "Hallo" + gap + firstName + gap + familyName +"!";
-    return outputStr; // ---> Daten zum Call
-    console.log("Hallo"); // return BEENDET die Funktion!
-}
-
-// 2. Funktionalität: data output
-// ausgabe("Hallo");
-function ausgabe(outputData) {
-    console.log(outputData); 
+    console.log("Hi Mohammed");    
 }
